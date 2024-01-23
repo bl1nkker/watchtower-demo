038a1ae6be5bdfdcb0de28e6313b5a440e7c8728
bl1nkker <nurovich14@gmail.com>
2024-01-23 18:40:12 +0600

    feat: added simple server + docker configuration

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+});
+
+const PORT = 8080;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
